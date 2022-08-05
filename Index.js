c539e27b10bc29e6373e151733b693020c68616c
unknown <abdulkaderjilani120@gmail.com>
2022-08-05 18:32:53 +0600

    users data load

--- Index.js
+++ Index.js
@@ -2,6 +2,40 @@ const express = require('express')
 const app = express()
 const port =process.env.PORT || 5000
 
+const cors = require('cors');
+
+require('dotenv').config()
+const corsConfig = {
+    origin: true,
+    credentials: true,
+}
+// Middle ware
+app.use(cors())
+app.use(express.json())
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://akjilani:UKFw3nbDlCXJRSy9@cluster0.ehvoh0g.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+  
+  try {
+    await client.connect();
+    const collection = client.db("user").collection("userData");
+    app.post("/user" , async (req , res) =>{
+      const user = req.body      
+      
+      const result = await collection.insertOne(user)      
+      res.send(result)   
+    })
+    app.get("/users" , async(req , res) =>{
+      const result = await collection.find().toArray()
+      res.send(result)
+     })
+  }finally{
+
+  }
+}
+run().catch(console.dir);
 app.get('/', (req, res) => {
   res.send('Secqure Company')
 })
